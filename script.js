894c70d26f226dd5822ffc617eff22fefc3bb47f
Nikita Grygoriev - Kotinskiy <gaenfodai23@gmail.com>
2026-05-11 23:36:26 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+// 1
+
+// let symbol = prompt("Enter Symbol");
+// alert(symbol ** 2);
+
+// 2
+
+// let symbol1 = prompt("Enter symbol 1");
+// let symbol2 = prompt("Enter Symbol 2");
+// symbol1 = parseFloat(symbol1);
+// symbol2 = parseFloat(symbol2);
+// alert((symbol1 + symbol2) / 2);
+
+// 3
+
+// let symbol = prompt("Enter symbol");
+// alert(symbol * symbol);
+
+// 4
+
+// let symbol = prompt("enter km");
+// const result = 0.621371 * symbol;
+// alert(result);
+
+// 5
+
+// let num1 = prompt("enter num1");
+// let num2 = prompt("enter num2");
+// let plus = num1 + num2;
+// let minus = num1 - num2;
+// let mnoz = num1 * num2;
+// let divison = num1 / num2;
+// alert(`plus = ${plus}, minus = ${minus}, mnoz = ${mnoz}, divis ${divison}.`);
+
+// 6
+
+// let num1 = prompt("num1");
+// let num2 = prompt("num2");
+
+// let x = -num2 / num1;
+
+// alert(x);
+
+// 7
+
+// let a = prompt("num 1");
+// let b = prompt("num 2");
+
+// let ra = 24 - a;
+// let rb = 60 - b;
+
+// alert(`Time Left: ${ra}, ${rb}`);
+
+// 8
+
+// let a = prompt("num pls");
+// a = parseInt(a);
+// alert((a / 10) % 10);
